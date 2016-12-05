3bc880e825417e32b40b14a4c81f1cb6e85a102c
Justin Jungst <jjungstprogramming@gmail.com>
2016-12-05 15:23:00 -0600

    quick mailer fix

--- app/mailers/enrollment_mailer.rb
+++ app/mailers/enrollment_mailer.rb
@@ -1,6 +1,6 @@
 class EnrollmentMailer < ApplicationMailer
   def notify_admissions_team(student)
     @student = student
-    mail(from: "sandboxd035c758dbb54757b1b13d910d57a6b3.mailgun.org", to: "admissions@anyonecanlearntocode.com, bursar@anyonecanlearntocode.com", subject: "New Enrollment: #{@student.full_name}")
+    mail(from: "mg.anyonecanlearntocode.com", to: "admissions@anyonecanlearntocode.com, bursar@anyonecanlearntocode.com", subject: "New Enrollment: #{@student.full_name}")
   end
 end
