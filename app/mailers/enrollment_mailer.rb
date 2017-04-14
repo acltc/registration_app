41448b5300d46f453055e664256b9c87d275b01c
Jay Wengrow <jaywngrw@gmail.com>
2017-04-14 13:30:28 -0500

    new mailer from

--- app/mailers/enrollment_mailer.rb
+++ app/mailers/enrollment_mailer.rb
@@ -1,6 +1,6 @@
 class EnrollmentMailer < ApplicationMailer
   def notify_admissions_team(student)
     @student = student
-    mail(from: "mg.anyonecanlearntocode.com", to: "admissions@anyonecanlearntocode.com, bursar@anyonecanlearntocode.com", subject: "New Enrollment: #{@student.full_name}")
+    mail(from: "admissions@actualize.co", to: "admissions@anyonecanlearntocode.com, bursar@anyonecanlearntocode.com", subject: "New Enrollment: #{@student.full_name}")
   end
 end
