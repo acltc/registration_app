66a82049dd3e9a472fb87025803d1b2e833a2d54
Jay Wengrow <jaywngrw@gmail.com>
2018-11-11 10:00:34 -0600

    more info in email so as to make it easier in close.io to find emergency contact info

--- app/mailers/enrollment_mailer.rb
+++ app/mailers/enrollment_mailer.rb
@@ -1,6 +1,6 @@
 class EnrollmentMailer < ApplicationMailer
   def notify_admissions_team(student)
     @student = student
-    mail(from: "admissions@actualize.co", to: "admissions@anyonecanlearntocode.com, bursar@anyonecanlearntocode.com", subject: "New Enrollment: #{@student.full_name}")
+    mail(from: @student.email, to: "operations@actualize.co, admissions@anyonecanlearntocode.com", subject: "New Enrollment: #{@student.full_name}")
   end
 end
